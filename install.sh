e86089bb51ab73ca9c1a439368ccc4fa9d83138c
JimBo <jimboca3@gmail.com>
2024-11-10 15:58:50 -0800

    3.0.13 Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+rm -rf camect camect-py
 pip3 install -r requirements.txt --upgrade --user
--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,6 +1,6 @@
 
 """ Node classes used by the Camect Node Server. """
-VERSION = "3.0.11"
+VERSION = "3.0.13"
 from .BaseNode          import BaseNode
 from .DetectedObject    import DetectedObject
 from .Camera            import Camera
